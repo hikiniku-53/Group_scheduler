d29d935f1a2b0cdd5e329f7aff5db31aa3b8c0b3
EC2 Default User <sho.eps.chem@gmail.com>
2024-01-13 23:53:56 +0900

    registration_member_2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後のアクセス先指定
+  def after_sign_in_path_for(resource_or_scope)
+      # マイページへ
+      member_path(current_member.id)
+  end
+
+  # ログアウト後のアクセス先
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+    
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :is_admin, :group_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/top'
   devise_for :members
   # ルートパス
   root to: 'homes#top'
+
+  resources :members, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

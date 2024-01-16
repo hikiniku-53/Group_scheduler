e9b58ea3f5e93814efc850d2b7da6a6bdb9ba958
EC2 Default User <sho.eps.chem@gmail.com>
2024-01-16 23:58:34 +0900

    migration_of_Group

---
+++ db/migrate/20240116145157_create_groups.rb
@@ -0,0 +1,10 @@
+class CreateGroups < ActiveRecord::Migration[6.1]
+  def change
+    create_table :groups do |t|
+      t.string :group_name, null: false
+      t.timestamps
+    end
+
+    add_index :groups, :group_name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 2024_01_10_144300) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name", null: false
-    t.boolean "is_admin", default: false, null: false
-    t.bigint "group_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_members_on_email", unique: true

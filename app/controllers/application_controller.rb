fde39ac430f2999ef4404c662c3e31fce4f5fcde
EC2 Default User <sho.eps.chem@gmail.com>
2024-01-18 23:56:25 +0900

    Group create_1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :members
   # ルートパス
   root to: 'homes#top'
 
   resources :members, only: [:index, :show]
+  resources :groups, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
